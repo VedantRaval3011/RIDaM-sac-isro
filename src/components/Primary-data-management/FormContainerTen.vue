28afbebddbd92295f6f7098e7c0aa16681c5a36c
VedantRaval3011 <121101897+VedantRaval3011@users.noreply.github.com>
2024-04-15 05:19:54 +0530

    Add files via upload

---
+++ src/components/Primary-data-management/FormContainerTen.vue
@@ -0,0 +1,206 @@
+<template>
+  <div class="form-container-ten mb-96">
+    <div class="bottom-layer">
+      <div class="steps">
+        <span class="bg-gradient-to-r from-blue-100 to-blue-100 p-3.5 py-2 rounded-xl">11</span> of <span id="total-steps">12</span>
+      </div>
+      <div class="move-icons">
+        <i class="fa-solid fa-circle-chevron-left" @click="handleLeftClick"></i>
+        <i
+          id="right"
+          class="fa-solid fa-circle-chevron-left"
+          @click="handleRightClick"
+        ></i>
+      </div>
+    </div>
+    <div class="flex flex-col w-full gap-5 text-center rounded-3xl drop-shadow-md backdrop-blur box-border p-7 bg-gradient-to-r from-blue-100 to-blue-100 container white">
+      <div class="font-semibold"><h3>Abstract describing the data</h3></div>
+      <div class="dia">
+        <label for="dia">Data identification abstract</label>
+        <div class="dia-input">
+          <input
+            type="dia"
+            id="dia"
+            class="w-96 h-10 rounded-3xl bg-blue-50 shadow-md border-none text-base p-4 pr-11 box-border"
+            v-model="dia"
+            @input="ValidateDia"
+          />
+          <i
+            v-if="CorrectDia && dia"
+            class="fa-solid fa-circle-check"
+            id="tick"
+          ></i>
+          <i
+            v-else-if="!CorrectDia && dia"
+            class="fa-solid fa-circle-xmark"
+            id="cross"
+          ></i>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import { ref,watch } from "vue";
+import useValidate from "@vuelidate/core";
+import jsonData from "../../../data.json";
+export default {
+  props:["initialData","id"],
+  setup(props, { emit }) {
+    const v$ = useValidate();
+    const dia = ref("");
+    const CorrectDia = ref(false);
+
+    watch(() => props.initialData, (newVal, oldVal) => {
+      if (newVal) {
+        // Update the values of the form inputs with the initial data
+        dia.value = newVal.dia;
+        
+      }
+    }, { immediate: true });
+
+    if(props.initialData && props.initialData.id){
+      CorrectDia.value = true;
+    const selectedData = jsonData.find(item => item.id === props.initialData.id);
+    if (selectedData) {
+      // Accessing values from the selected data
+      const jsonDia = selectedData.metadata.dia;
+      
+      // Setting initial values based on JSON data
+      dia.value = jsonDia;
+      
+      
+      console.log("dia:," , dia.value);
+    } else {
+      console.error('Data for the provided id not found.');
+    }
+    }
+
+    const ValidateDia = () => {
+      CorrectDia.value = dia.value.length > 1;
+    };
+
+    const handleRightClick = () => {
+     
+        const data = {
+        dia: dia.value,
+        CorrectDia: CorrectDia.value,
+      };
+
+      emit("update-data", data);
+        emit("validate-and-proceed-form-ten");
+      
+    };
+
+    const handleLeftClick = () =>{
+      emit("MoveBackToNine");
+    }
+
+    return {
+      dia,
+      v$,
+      handleRightClick,
+      ValidateDia,
+      CorrectDia,
+      handleLeftClick
+    };
+  },
+};
+</script>
+<style scoped>
+body {
+  padding: 0;
+  margin: 0;
+}
+
+
+.dia-input {
+  position: relative;
+}
+
+#tick {
+  color: green;
+  position: absolute;
+   top: 0.7em;
+  right: 1em;
+}
+
+#cross {
+  color: crimson;
+  position: absolute;
+   top: 0.7em;
+  right: 1em;
+}
+
+.bottom-layer {
+  width: 100%;
+  padding: 1em;
+  text-align: center;
+  font-size: 1.5rem;
+  display: flex;
+  gap: 0.3em;
+  margin-left: 4.5em;
+}
+
+#right {
+  transform: rotate(180deg);
+  position: relative;
+  left: 0.1em;
+}
+
+.dia {
+  display: flex;
+  flex-direction: column;
+  gap: 0.3em;
+}
+
+@media screen and (max-width: 640px) {
+  /* Small mobile devices */
+  .white{
+    width: 120%;
+  }
+  .form-container-ten{
+    margin-left: 2.5rem;
+  }
+
+  .bottom-layer{
+    margin-left: 3em;
+  }
+  #dia{
+    width: 100%;
+  }
+  
+ 
+
+}
+
+@media screen and (min-width: 641px) and (max-width: 767px) {
+  /* Big mobile devices */
+ 
+}
+
+@media screen and (min-width: 768px) and (max-width: 1023px) {
+  /* Small laptops and tablets */
+ 
+}
+
+@media screen and (min-width: 1024px) and (max-width: 1279px) {
+  /* Big laptops and desktops */
+  
+}
+@media screen and (min-width: 1280px) and (max-width: 1440px) {
+  /* Big laptops and desktops */
+
+}
+@media screen and (min-width: 1441px) {
+  /* Big laptops and desktops */
+
+}
+
+
+
+
+
+</style>
---
+++ src/views/ridam-management-module/ridam.vue
@@ -0,0 +1,112 @@
+<template >
+  <div class="main flex flex-col gap-1 w-screen h-screen bg-gradient-to-b from-blue-300 to-blue-200 overflow-y-hidden overflow-x-auto">
+    <div class="top-container">
+      <div class="logo w-40">
+        <img src="../../assets/vedas.png" alt="vedas logo" />
+      </div>
+      <div class="leftmost-icons flex justify-around gap-3">
+        <div class="back-button shadow-2xl rounded-full cursor-pointer mt-10 px-2 h-10 flex items-center justify-center" @click="$router.push('/')">
+          <i class="fa-solid fa-circle-left text-4xl"></i>
+        </div>
+        <div class="home w-10 h-10 text-1xl rounded-full mt-10 px-2 cursor-pointer flex items-center justify-center" @click="$router.push('/')">
+          <i class="fa-solid fa-house "></i>
+        </div>
+        <div class="user">
+          <img class="user-button" src="../../assets/userbg.png" alt="user-image" />
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col items-center justify-center flex-1">
+      <h1 class="text-4xl font-bold mb-2">RIDaM</h1>
+      <div class="flex flex-col items-center mb-4">
+        
+        <div class="pill bg-blue-100 rounded-full p-3 shadow-md mt-3">
+          <p class="text-lg font-semibold">Raster Analytics System for Interactive Data Modeling</p>
+        </div>
+      </div>
+      <h2 class="text-xl font-semibold mb-4">Select the management module</h2>
+      <ScrollableModule scrollDistance="3000" />
+    </div>
+    <div class="flex justify-end p-4 bg-blue-100">
+      <p class="text-lg font-medium">About RIDaM</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import ScrollableModule from "../../components/Ridam-Pallete/ScrollableModule.vue";
+export default {
+  name: "Ridam",
+  components: {
+    ScrollableModule,
+  },
+};
+</script>
+
+<!-- No scoped styles needed, as Tailwind CSS is utility-based -->
+<style scoped>
+  /* Media Queries */
+
+  .top-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 0;
+  
+  display: flex;
+  justify-content: space-between;
+}
+.back-button{
+  color: #2756ff;
+}
+
+.logo {
+  margin-right: 2em; /* Add some spacing between the logo and other content */
+}
+
+.home{
+  background-color: #2756ff;
+  color: #97c7fd;
+}
+
+.logo img {
+  margin-left: 1em;
+}
+
+.user-button {
+  width: 5rem;
+  margin-top: 1rem;
+  margin-right: 2rem;
+  border-radius: 50%; /* Add rounded corners */
+  box-shadow: 0px 1px 5px rgb(94, 77, 77);
+}
+@media screen and (max-width: 640px) {
+  /* Small mobile devices */
+ 
+
+}
+
+@media screen and (min-width: 641px) and (max-width: 767px) {
+  /* Big mobile devices */
+ 
+}
+
+@media screen and (min-width: 768px) and (max-width: 1023px) {
+  /* Small laptops and tablets */
+ 
+}
+
+@media screen and (min-width: 1024px) and (max-width: 1279px) {
+  /* Big laptops and desktops */
+  
+}
+@media screen and (min-width: 1280px) and (max-width: 1440px) {
+  /* Big laptops and desktops */
+
+}
+@media screen and (min-width: 1441px) {
+  /* Big laptops and desktops */
+
+}
+</style>
